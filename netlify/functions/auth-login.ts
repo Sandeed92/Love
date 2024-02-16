4f6d699f1ecee6431483c97fe1a9d444836e4ae4
Mohd Sandeed <155838071+Sandeed92@users.noreply.github.com>
2024-02-16 21:47:29 +0530

    added netlify

---
+++ netlify/functions/auth-login.ts
@@ -0,0 +1,35 @@
+import { Handler } from '@netlify/functions';
+import { getWebFlowAuthorizationUrl } from '@octokit/oauth-methods';
+import { serialize } from 'cookie';
+
+export const handler: Handler = async (event) => {
+
+  let returnRoute = "/";
+  if(event.headers.referer) {
+    returnRoute = event.headers.referer;
+  };
+
+  const redirectCookie = serialize('nf-authed-path',  JSON.stringify({route: returnRoute}), {
+    secure: true,
+    httpOnly: true,
+    sameSite: true,
+    maxAge: 1000 * 60 * 60  // one hour
+  });
+
+  const { url } = await getWebFlowAuthorizationUrl({
+    clientType: 'oauth-app',
+    clientId: process.env.GITHUB_APP_CLIENT_ID,
+    scopes: ['read:user', 'read:org'],
+  });
+
+  console.log(`getWebFlowAuthorizationUrl: ${url}`)
+
+  return {
+    statusCode: 301,
+    headers: {
+      'Set-Cookie': redirectCookie,
+      Location: url,
+    },
+    body: JSON.stringify({ status: 'Redirecting...' }),
+  };
+};
---
+++ netlify/functions/user.ts
@@ -0,0 +1,35 @@
+import NetlifyGraph from './netlifyGraph';
+import { parse } from 'cookie';
+
+export const handler = async (event) => {
+  const [, , , username = 'philhawksworth'] = event.path.split('/');
+  const cookies = parse(event.headers.cookie);
+  const auth = JSON.parse(cookies['nf-gh-session']);
+
+  const { errors: GitHubDataErrors, data: GitHubDataData } =
+    await NetlifyGraph.fetchGitHubData({
+      login: username,
+      gitHubOAuthToken: auth.access_token,
+    });
+
+  if (GitHubDataErrors) {
+    console.error(JSON.stringify(GitHubDataErrors, null, 2));
+    return {
+      statusCode: 500,
+      body: JSON.stringify(GitHubDataErrors),
+      headers: {
+        'content-type': 'application/json',
+      },
+    };
+  }
+
+  const user = GitHubDataData.gitHub.search?.nodes?.[0];
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(user),
+    headers: {
+      'content-type': 'application/json',
+    },
+  };
+};
